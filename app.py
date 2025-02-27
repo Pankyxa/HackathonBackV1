eb2dad875d54accb106fae38dc8474f0aeb8a23a
Pankyxa <u3@tyuiu.ru>
2025-02-27 14:34:33 +0500

    fix: prepare for production version fixes

--- app.py
+++ app.py
@@ -18,12 +18,15 @@ app = FastAPI(
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[
+        "https://hackathon.tyuiu.ru",
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth_router)
 app.include_router(teams_router)
 app.include_router(users_router)
